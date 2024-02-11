a249ecadb9bf503f0d84fc4377acae3d6209c696
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-02-12 01:57:31 +0330

    improve/ add fetch cars part

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import { CustomFilter, Hero, SearchBar } from '@/components';
-import Image from 'next/image';
+'use client';
+import { CarCard, Hero, SearchBar } from '@/components';
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1;
+
+  console.log(allCars, isDataEmpty);
+
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -19,6 +25,21 @@ export default function Home() {
             <CustomFilter title='year' /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className='home_error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no result!!!</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
